67c68476ebeb6998fede512e018f9e883e1eeee8
KhathirulSan <paturihsan4@gmail.com>
2023-04-12 22:19:58 +0700

    Menambahkan category serta menambahkan validasi js

--- script.js
+++ script.js
@@ -14,15 +14,30 @@ function bmi_check_number(number) {
       let status;
       if(bmi>=30){
         status = "Obesity"
-      }else if(bmi>=25 && bmi<=30){
+      }else if(bmi>=25 && bmi<=29.9){
         status = "Overweight"
       }
-      else if (bmi>=18.5 && bmi<=25){
+      else if (bmi>=18.5 && bmi<=24.9){
         status = "Normal Weight"
       }
       else {
         status = "Underweight"
       }
-      document.getElementById("output").innerHTML = "Your BMI is <b>"+ bmi +"</b> which means You are <b>"+status+"</b>";
+      document.getElementById("output").innerHTML = "Your Result BMI is <b>"+ bmi +"</b> which means You are <b>"+status+"</b>";
     }
   }
+
+//   Validasi Form
+const weight = document.getElementById("weight");
+const height = document.getElementById("height");
+const btn = document.getElementsByTagName('button')[0];
+
+btn.addEventListener("click", function(){
+    if(weight.value == "" && height.value == ""){ 
+        alert("Input Weight dan Height Tidak Boleh Kosong!");
+    }else if(weight.value == ""){
+        alert("Input Weight Tidak Boleh Kosong!");
+    }else if(height.value == ""){
+        alert("Input Height Tidak Boleh Kosong!");
+    }
+})
